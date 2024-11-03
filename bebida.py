e0798ccaa62a2d73d977dfaac8099f652da984f6
João Victor <tkirito940@gmail.com>
2024-11-03 18:58:11 -0400

    classe bebida
    classe bebida e construtor

--- bebida.py
+++ bebida.py
@@ -1,3 +1,16 @@
-print("joao e lindo")
+class Bebida:
+    def __init__(self,nome,descricao,valor,quantidade,ml):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+        self.quantidade = quantidade
+        self.ml = ml
 
-print ("joao comeu quem leu viu")
+def exibir_detalhes (self):
+    print(f"Bebida: {self.nome}")
+    print(f"descricao:{self.descricao}")
+    print(f"valor: R${self.valor:.2f}")
+    print(f"quantidade em estoque: {self.quantidade}")
+    print(f"volume:{self.ml} ml")
+
+bebida1 = Bebida ("refrigerante", 'refrigerante de cola', 5.00 , 10 , 350)
